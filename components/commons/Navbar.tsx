e0cdb1e109d0b3688118045df837b8dd027a8291
Oscar <tangqh@sea.com>
2024-06-19 16:33:44 +0800

    feat: click navbar to back to home

--- components/commons/Navbar.tsx
+++ components/commons/Navbar.tsx
@@ -1,10 +1,16 @@
 import { SettingsButton } from '@components/settings/SettingsButton';
 
 export const Navbar = () => {
+  const goHome = () => {
+    window.location.href = '/';
+  };
+
   return (
     <div className="navbar bg-base-100 px-0 pt-0 min-h-[auto]">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">💻👨‍✈️ Code Pilot</a>
+        <a className="btn btn-ghost text-xl" onClick={goHome}>
+          💻👨‍✈️ Code Pilot
+        </a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
