5dff8313b101128511fcdc09ee2f9c091a425c22
Oscar <tangqh@sea.com>
2024-06-19 15:17:50 +0800

    chore: update navbar height

--- components/commons/Navbar.tsx
+++ components/commons/Navbar.tsx
@@ -2,7 +2,7 @@ import { SettingsButton } from '@components/settings/SettingsButton';
 
 export const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 px-0">
+    <div className="navbar bg-base-100 px-0 pt-0 min-h-[auto]">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">💻👨‍✈️ Code Pilot</a>
       </div>
