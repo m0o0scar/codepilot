e2d41234dafeb306c21f13c2ed1ad91ca72f3066
Oscar <tangqh@sea.com>
2024-06-28 12:10:10 +0800

    feat(source code): remove inline base64 images and svgs to save some token

--- utils/sourceCode.ts
+++ utils/sourceCode.ts
@@ -57,6 +57,29 @@ export const excludeFilePattern = new RegExp(
     .join('|'),
 );
 
+const removeBase64Images = (content: string) => {
+  return content.replaceAll(/(["'`])data:image\/.*;base64,.*(["'`])/g, '$1data:image...$2');
+};
+
+const removeSvgs = (content: string) => {
+  return content.replaceAll(/<svg\b[^>]*>[\s\S]*?<\/svg>/gim, '<svg>...</svg>');
+};
+
+const seq = (input: string, processors: ((input: string) => string)[]) => {
+  let output = input;
+  for (const processor of processors) {
+    output = processor(output);
+  }
+  return output;
+};
+
+const processSourceCodeContent = (content: string, ext: string) => {
+  if (['js', 'mjs', 'jsx', 'ts', 'tsx', 'html'].includes(ext)) {
+    return seq(content, [removeBase64Images, removeSvgs]);
+  }
+  return content;
+};
+
 export const readSourceFileContents = async (
   files: Entry[],
   options?: {
@@ -70,29 +93,25 @@ export const readSourceFileContents = async (
 
   const contents = await Promise.all(
     files.map(async (e) => {
+      // get file ext name and full file path
+      const ext = last(e.filename.split('.')) || '';
+      const filename = processFileName ? await processFileName(e.filename) : e.filename;
+
       // read file content
       const blob = await e.getData!(new BlobWriter());
-      const raw = new TextDecoder().decode(await blob.arrayBuffer());
+      let text = new TextDecoder().decode(await blob.arrayBuffer());
+
+      // process file content based on their type
+      text = processSourceCodeContent(text, ext);
 
       // calculate the number of lines
-      const lines = raw
+      const lines = text
         .split('\n')
         .map((l) => l.trim())
         .filter(Boolean);
       totalNumberOfLines += lines.length;
 
-      // TODO remove function implementation
-      // TODO add line numbers to the source code
-      const text = raw;
-
-      // get file ext name and full file path
-      const ext = last(e.filename.split('.')) || '';
-      const filename = processFileName ? await processFileName(e.filename) : e.filename;
-
-      // create a code block
-      const block = `${filename}:\n\n\`\`\`${ext}\n${text}\n\`\`\``;
-
-      return { filename, block, text };
+      return { ext, filename, text };
     }),
   );
 
@@ -101,7 +120,9 @@ export const readSourceFileContents = async (
   const tree = prettyTree(filePaths);
 
   // combine all source code
-  const combinedSourceCode = contents.map(({ block }) => block).join('\n\n');
+  const combinedSourceCode = contents
+    .map(({ ext, filename, text }) => `${filename}:\n\n\`\`\`${ext}\n${text}\n\`\`\``)
+    .join('\n\n');
 
   return {
     tree,
